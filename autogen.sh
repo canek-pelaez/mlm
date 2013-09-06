34b72441c3d4b314dbcce17fdd55782bdd5937d2
Canek Peláez <canek@ciencias.unam.mx>
2013-09-06 13:01:41 -0500

    Added autogen.sh.

---
+++ autogen.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+set -e # exit on errors
+
+srcdir=`dirname $0`
+test -z "$srcdir" && srcdir=.
+
+mkdir -p m4
+touch ChangeLog
+
+autoreconf -v --force --install
+#intltoolize -f -c
+
+if [ -z "$NOCONFIGURE" ]; then
+    "$srcdir"/configure $@
+fi
