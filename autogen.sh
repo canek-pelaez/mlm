5505da8e977879f7f06e4b48788963496cf4e2d2
Canek Peláez Valdés <canek@ciencias.unam.mx>
2014-09-07 02:19:21 -0500

    build-sys: Don't ignore ChangeLog, and get rid of INSTALL.

--- autogen.sh
+++ autogen.sh
@@ -2,15 +2,14 @@
 
 set -e # exit on errors
 
-srcdir=`dirname $0`
-test -z "$srcdir" && srcdir=.
+srcdir=$(dirname $0)
+test -z "${srcdir}" && srcdir=.
 
 mkdir -p m4
-touch ChangeLog
 
 autoreconf -v --force --install
 intltoolize -f -c
 
-if [ -z "$NOCONFIGURE" ]; then
-    "$srcdir"/configure $@
+if [ -z "${NOCONFIGURE}" ]; then
+    "${srcdir}"/configure $@
 fi
